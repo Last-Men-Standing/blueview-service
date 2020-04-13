eb137c9a5e5311153e7163ebc8e1ec7244eb28ac
Will Zawilinski <will.zawilinski@gmail.com>
2020-04-13 02:00:52 -0400

    Tinkering with cors

--- src/controllers/department/department_router.js
+++ src/controllers/department/department_router.js
@@ -1,10 +1,12 @@
 "use strict";
 const express = require("express");
+const cors = require("cors");
 // Import controller functions
 const { verifyToken } = require("../../middleware/authorization");
 const { getDepartmentbyId, getDepartmentbyZipcode, getDepartments, createPost, getDepartmentPosts } = require("./department_controller");
 const departmentRouter = express.Router();
 
+departmentRouter.options("*", cors());
 departmentRouter.get("/all", getDepartments);
 departmentRouter.get("/zipcode/:zipcode", getDepartmentbyZipcode);
 departmentRouter.get("/:id", getDepartmentbyId);
--- src/controllers/user/account_router.js
+++ src/controllers/user/account_router.js
@@ -1,9 +1,11 @@
 "use strict";
 const express = require("express");
+const cors = require("cors");
 // Import controller functions
 const { createAccount, login, getById } = require("./account_controller");
 const accountRouter = express.Router();
 
+accountRouter.options("*", cors());
 accountRouter.post("/register", createAccount);
 accountRouter.post("/login", login);
 accountRouter.get("/:id", getById)
